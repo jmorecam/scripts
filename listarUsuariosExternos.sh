0e49a85082a9f84b99dd9c873385b595f5bb7407
Diego Muñoz Beltrán <diego.munoz@beeva.com>
2017-10-11 15:56:15 +0200

    little script

---
+++ listarUsuariosExternos.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+if [ -z "$1" ] || [ -z "$2" ]; then
+
+  echo -e '\nFuncionamiento-> ./listarUsuariosExternos.sh <nombreDeUsuario> <nombreFichero>\n'
+
+else
+
+  rutaHome=/home/$1/
+  for i in *; do
+     if grep -q -v '@beeva.com' <<< "$i"; then
+         echo -e '*********************************************************' >> $rutaHome$2
+         echo $i >> $rutaHome$2
+         cat $i >> $rutaHome$2
+         echo -e '*********************************************************\n\n' >> $rutaHome$2
+     fi
+  done
+  chown $1:$1 $rutaHome$2
+
+fi
